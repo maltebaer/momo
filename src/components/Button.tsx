35a160372d15da6cc252a8ef005ac13fedd69195
Malte <m.felmy@googlemail.com>
2020-04-28 22:16:24 +0200

    Added animations.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
 
+import specsUrl from "../assets/specs.svg";
+import parentsUrl from "../assets/parents.svg";
+import timeUrl from "../assets/time.svg";
+
 import {BackContent} from "./Home";
 
 interface ButtonProps {
@@ -10,51 +14,14 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = (props) => {
     const renderIcon = (): JSX.Element => {
         switch (props.id) {
-            case "parents":
-                return (
-                    <svg
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        className="icon"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                            clipRule="evenodd"
-                        ></path>
-                    </svg>
-                );
-
             case "specs":
-                return (
-                    <svg
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        className="icon"
-                    >
-                        <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
-                        <path
-                            fillRule="evenodd"
-                            d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-                            clipRule="evenodd"
-                        ></path>
-                    </svg>
-                );
+                return <img className="icon" src={specsUrl} alt="Specs" />;
+
+            case "parents":
+                return <img className="icon" src={parentsUrl} alt="Parents" />;
 
             case "time":
-                return (
-                    <svg
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        className="icon"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-                            clipRule="evenodd"
-                        ></path>
-                    </svg>
-                );
+                return <img className="icon" src={timeUrl} alt="Time" />;
 
             default:
                 return null;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
+import {useSpring, animated as a} from "react-spring";
 
 import momoUrl from "../assets/hugo-steffi.jpg";
 import parentsUrl from "../assets/hugo-steffi-2.jpg";
+import timeUrl from "../assets/time.jpg";
 
 import Card from "./Card";
 import Button from "./Button";
@@ -9,7 +11,7 @@ import Button from "./Button";
 const Name: React.FC = () => {
     return (
         <div className="card py-2">
-            <h2 className="text-xl text-blue-800 font-bold">Hugo Lovis Bär</h2>
+            <h2 className="text-xl text-blue-800 font-bold">Hugo Rakete Bär</h2>
             <div className="text-gray-800 text-right font-semibold">
                 4. Juni 2020
             </div>
@@ -22,21 +24,55 @@ interface FrontProps {
 }
 
 const Front: React.FC<FrontProps> = (props) => {
+    const [position, set] = useSpring(() => ({
+        xy: [0, 0],
+        config: {mass: 10, tension: 550, friction: 140},
+    }));
+
+    const calc = (x: number, y: number): number[] => [
+        x - window.innerWidth / 2,
+        y - window.innerHeight / 2,
+    ];
+
+    const translate = (x: number, y: number): string => {
+        const strength = 40;
+        return `translate3d(${-x / strength}px, ${-y / strength}px, 0)`;
+    };
+
     return (
-        <div className="mx-auto max-w-lg">
+        <div
+            className="mx-auto max-w-lg"
+            onMouseMove={({clientX: x, clientY: y}): void =>
+                set({xy: calc(x, y)})
+            }
+        >
             <div className="relative flex justify-between">
-                <Button id="specs" handleTurn={props.handleTurn} />
-                <Button id="parents" handleTurn={props.handleTurn} />
+                <a.div
+                    style={{
+                        transform: position.xy.interpolate(translate),
+                    }}
+                >
+                    <Button id="specs" handleTurn={props.handleTurn} />
+                </a.div>
+                <a.div
+                    style={{
+                        transform: position.xy.interpolate(translate),
+                    }}
+                >
+                    <Button id="parents" handleTurn={props.handleTurn} />
+                </a.div>
             </div>
             <div className="px-6">
                 <img
                     className="-mt-8 object-cover shadow-md"
                     src={momoUrl}
-                    alt="Hugo"
+                    alt="Momo"
                 />
             </div>
             <div className="-mt-10 flex justify-between items-end">
-                <Button id="time" handleTurn={props.handleTurn} />
+                <a.div style={{transform: position.xy.interpolate(translate)}}>
+                    <Button id="time" handleTurn={props.handleTurn} />
+                </a.div>
                 <Name />
             </div>
         </div>
@@ -48,6 +84,12 @@ export type BackContent = "parents" | "specs" | "time" | null;
 const Home: React.FC = () => {
     const [back, setBack] = React.useState<BackContent>(null);
 
+    const {transform, opacity} = useSpring({
+        opacity: back ? 1 : 0,
+        transform: `perspective(600px) rotateX(${back ? 180 : 0}deg)`,
+        config: {mass: 4, tension: 180, friction: 30},
+    });
+
     const handleTurn = (content: BackContent): void => {
         setBack(content);
     };
@@ -58,22 +100,41 @@ const Home: React.FC = () => {
 
     const renderBack = (): JSX.Element => {
         switch (back) {
-            case "parents":
-                return <img src={parentsUrl} alt="Steffi und Malte" />;
             case "specs":
                 return <div>Specs</div>;
+            case "parents":
+                return <img src={parentsUrl} alt="Steffi und Malte" />;
             case "time":
-                return <div>Time</div>;
+                return <img src={timeUrl} alt="Pow" />;
 
             default:
                 return null;
         }
     };
 
-    return back ? (
-        <Card onClose={handleClose}>{renderBack()}</Card>
-    ) : (
-        <Front handleTurn={handleTurn} />
+    return (
+        <div className="flex justify-center items-center">
+            <a.div
+                className="c absolute"
+                style={{
+                    opacity: opacity.interpolate((o: number) => 1 - o),
+                    transform,
+                }}
+            >
+                <Front handleTurn={handleTurn} />
+            </a.div>
+            <a.div
+                className="c absolute"
+                style={{
+                    opacity,
+                    transform: transform.interpolate(
+                        (t) => `${t} rotateX(180deg)`,
+                    ),
+                }}
+            >
+                <Card onClose={handleClose}>{renderBack()}</Card>
+            </a.div>
+        </div>
     );
 };
 
