48e9862a27400d30bc7a01fb6a56610cf19e3aed
Malte Bär <m.felmy@lum-gmbh.de>
2020-05-11 17:08:45 +0200

    Updated Tailwind usage.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import "./tailwind.pcss";
+
 import App from "./components/App";
 
 const root = document.getElementById("root");
